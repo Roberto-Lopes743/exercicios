b65c1acb5946f699187d5f2e0689bd08e4e02056
SapoSapeca <robertolopes2006321@gmail.com>
2025-05-04 22:49:06 -0300

    Add files via upload

---
+++ python/inverterTexto.py
@@ -0,0 +1,11 @@
+#minha resolução
+txt = 'texto'
+txt_invertida = txt[::-1]
+print(txt_invertida)
+
+#GPT
+def inverter_string(texto):
+    return texto[::-1]
+
+# Exemplo de uso:
+print(inverter_string("python"))  # Saída: "nohtyp"
